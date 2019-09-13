65e52af93b0e3d0ebbee4ede3df5ddaa544ca3fe
ykanane <32992959+ykanane@users.noreply.github.com>
2019-09-13 00:22:06 -0400

    Modified solution to use the concept of memoization to avoid duplicate work

--- HFM.py
+++ HFM.py
@@ -1,24 +1,42 @@
 import sys
 
-'''Naive mutually recursive solution, this is extremely slow. Will
-optimize using memoization'''
-def female(n):
+'''This solution uses the concept of memoization to avoid
+duplicate work
+'''
+
+def female(n, memo):
+    """ Create unique key for each value of N and store result in the hashmap
+    passed as an argument ("memo"). Check at the beginning of the function if this key
+    already exists in the map. If so, we already computed this so return the
+    value immediately.
+    """
+    key = "F" + str(n)
+    if key in memo:
+        return memo[key]
     if n == 0:
         return 1
-    return  n -  male(female(n - 1))
+    ret_val = n -  male(female(n - 1, memo), memo)
+    memo[key] = ret_val
+    return ret_val
 
-def male(n):
+def male(n, memo):
+    key = "M" + str(n)
+    if key in memo:
+        return memo[key]
     if n == 0:
         return 0
-    return n - female(male(n-1))
+    ret_val = n - female(male(n-1, memo), memo)
+    memo[key] = ret_val
+    return ret_val
 
 if __name__ == "__main__":
     n = sys.argv[1]
     flist = []
     mlist = []
+    memo = dict()
 
     for i in range(0,int(n)):
-        flist.append(female(i))
-        mlist.append(male(i))
+        flist.append(female(i, memo))
+        mlist.append(male(i, memo))
     print("F: {}".format(flist))
     print("M: {}".format(mlist))
